b60c73b967afe9f4e4cfc1c2e8bcb485b060ffce
lowelv7 <terrylv7@hotmail.com>
2018-08-16 00:54:10 +0800

    code clean up

--- public/hello.js
+++ public/hello.js
@@ -3,7 +3,15 @@ var app = angular.module('demo', []);
 
 app.controller('Search', function($scope,$http) {
 
+	$scope.search = function (name) {
+		    $http.get('http://localhost:8888/bookmanage/search?name='+name).
+		    then(function(response) {
+            $scope.books = response.data;
+        });
 
+	}
+
+	//auto load library books by default
     $http.get('http://localhost:8888/bookmanage/search?name=*').
         then(function(response) {
             $scope.books = response.data;
@@ -12,12 +20,12 @@ app.controller('Search', function($scope,$http) {
 });
 
 
-app.controller('Read', function($scope, $http) {
-    $http.get('http://localhost:8888/bookmanage/read?id=1&name=2').
-        then(function(response) {
-            $scope.book = response.data;
-        });
-});
+// app.controller('Read', function($scope, $http) {
+//     $http.get('http://localhost:8888/bookmanage/read?id=1&name=2').
+//         then(function(response) {
+//             $scope.book = response.data;
+//         });
+// });
 
 app.controller('Create', function($scope, $http) {
 
@@ -25,8 +33,5 @@ app.controller('Create', function($scope, $http) {
 		    $http.get('http://localhost:8888/bookmanage/create?id='+id+'&name='+name+'&author='+author);
 
 	}
-    $http.get('http://localhost:8888/bookmanage/create?id=4&name=4book&author=4a').
-        then(function(response) {
-            $scope.book2 = response.data;
-        });
+    
 });
