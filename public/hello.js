0d95a09605abdb76492b91b4301f4303b50eb57e
lowelv7 <terrylv7@hotmail.com>
2018-08-16 02:15:40 +0800

    book delete function added to frontend

--- public/hello.js
+++ public/hello.js
@@ -1,15 +1,36 @@
 var app = angular.module('demo', []);
 
 
-app.controller('Search', function($scope,$http) {
+app.controller('ListController', function($scope,$http) {
 
-	$scope.search = function (name) {
+$scope.idSelectedBook = null;
+$scope.setSelected = function (idSelectedBook) {
+   $scope.idSelectedBook = idSelectedBook;
+}
+	
+
+$scope.search = function (name) {
 		    $http.get('http://localhost:8888/bookmanage/search?name='+name).
 		    then(function(response) {
             $scope.books = response.data;
         });
 
+}
+
+$scope.createBook = function (id,name,author) {
+		    $http.get('http://localhost:8888/bookmanage/create?id='+id+'&name='+name+'&author='+author);
+
 	}
+
+$scope.delete = function (id) {
+	if($scope.idSelectedBook!=null){
+		    $http.get('http://localhost:8888/bookmanage/delete?id='+id);
+		    // then(function(response) {
+      //       // $scope.books = response.data;
+      //   });
+		}
+
+}
 
 	//auto load library books by default
     $http.get('http://localhost:8888/bookmanage/search?name=*').
